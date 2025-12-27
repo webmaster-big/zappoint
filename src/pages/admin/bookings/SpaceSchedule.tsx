acc92d9d3fa57081e466657adb16111d13540fa5
webmaster-big <webmaster@bestingames.com>
2025-12-27 22:47:35 +0800

    Replace legend with hover tooltip for cleaner UI

--- src/pages/admin/bookings/SpaceSchedule.tsx
+++ src/pages/admin/bookings/SpaceSchedule.tsx
@@ -357,15 +357,16 @@ const SpaceSchedule = () => {
             </StandardButton>
           </div>
 
-          <StandardButton
+         
+
+          {/* Legend Tooltip */}
+          <div className="relative group">
+             <StandardButton
             variant="primary"
             onClick={goToToday}
           >
             Today
           </StandardButton>
-
-          {/* Legend Tooltip */}
-          <div className="relative group">
             <button className="p-2 text-gray-500 hover:text-gray-700 hover:bg-gray-100 rounded-lg transition">
               <Info className="w-5 h-5" />
             </button>
